1ee037416772554b8827e38bc47995325d468232
Wizardbacon13 <longphan0619@gmail.com>
2024-10-30 21:14:13 -0500

    new9

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import classnames from "classnames";
-console.log("new8");
+console.log("new9");
 
 const NavBar = () => {
   const currentPath = usePathname();
