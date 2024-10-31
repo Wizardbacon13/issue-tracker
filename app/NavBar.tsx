68cedde5eb03e5eb8c2b778392f65de600ba31ba
Wizardbacon13 <longphan0619@gmail.com>
2024-10-30 21:29:26 -0500

    remove pls

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import classnames from "classnames";
+console.log("remove pls")
 
 const NavBar = () => {
   const currentPath = usePathname();
